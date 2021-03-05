ef194189a122d84e3154c0a186539757d2c60b29
Cody <xpythonxx5@gmail.com>
2021-03-04 19:00:14 -0500

    Database Creation and USER table creation statements added

--- InitializeDatabase.sql
+++ InitializeDatabase.sql
@@ -1,1 +1,14 @@
 /*Statements to create database and tables**/
+
+/*Creates Database**/
+CREATE DATABASE TruckTracker;
+
+/*Create USER table**/
+CREATE TABLE USER(
+  UserID int NOT NULL AUTO_INCREMENT,
+  FirstName varchar(30) NOT NULL,
+  LastName varchar(30) NOT NULL,
+  HashPwd varchar(64) NOT NULL,
+  UserType varchar(8) NOT NULL,
+  CONSTRAINT UserPK PRIMARY KEY(UserID)
+);
