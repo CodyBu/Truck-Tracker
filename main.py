f5994702608ca978eccd3e45180d2ee80ba8d29b
Cody <xpythonxx5@gmail.com>
2021-03-11 10:19:11 -0500

    Rerranges folders and fixes issue when adding data to the database

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
+from hashlib import sha256
 import re
 
 app = Flask(__name__)
 
-# Change this to your secret key (can be anything, it's for extra protection)
+# Change this to your sepcret key (can be anything, it's for extra protection)
 app.secret_key = 'group4'
 
 # Enter your database connection details below
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'smartsQl4'
 app.config['MYSQL_DB'] = 'TruckTracker'
 
 # Intialize MySQL
@@ -23,20 +24,20 @@ def login():
     # Output message if something goes wrong...
     msg = ''
     # Check if "username" and "password" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+    if request.method == 'POST' and 'username' and 'password' in request.form:
         # Create variables for easy access
         username = request.form['username']
-        password = request.form['password']
+        hashPassword = sha256(request.form['password'].encode()).hexdigest()
         # Check if account exists using MySQL
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM USER WHERE UserName = %s AND HashPwd = %s', (username, password,))
+        cursor.execute('SELECT * FROM USER WHERE UserName = %s AND HashPwd = %s' % (username, hashPassword))
         # Fetch one record and return result
         account = cursor.fetchone()
         # If account exists in accounts table in out database
         if account:
             # Create session data, we can access this data in other routes
             session['loggedin'] = True
-            # session['id'] = account['id']
+            session['id'] = account['id']
             session['UserName'] = account['UserName']
             return redirect(url_for('home'))
         else:
@@ -45,74 +46,14 @@ def login():
     # Show the login form with message (if any)
     return render_template('index.html', msg=msg)
 
-
 @app.route('/logout')
 def logout():
     # Remove session data, this will log the user out
-   session.pop('loggedin', None)
-   session.pop('id', None)
-   session.pop('UserName', None)
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('UserName', None)
    # Redirect to login page
-   return redirect(url_for('login'))
-
-
-# http://localhost:5000/Falsk/register - this will be the registration page, we need to use both GET and POST requests
-from flask import Flask, render_template, request, redirect, url_for, session
-from flask_mysqldb import MySQL
-import MySQLdb.cursors
-import re
-
-app = Flask(__name__)
-
-# Change this to your secret key (can be anything, it's for extra protection)
-app.secret_key = 'group4'
-
-# Enter your database connection details below
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'TruckTracker'
-
-# Intialize MySQL
-mysql = MySQL(app)
-
-# http://localhost:5000/pythonlogin/ - this will be the login page, we need to use both GET and POST requests
-@app.route('/', methods=['GET', 'POST'])
-def login():
-    # Output message if something goes wrong...
-    msg = ''
-    # Check if "username" and "password" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-        # Create variables for easy access
-        username = request.form['username']
-        password = request.form['password']
-        # Check if account exists using MySQL
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM USER WHERE UserName = %s AND HashPwd = %s', (username, password,))
-        # Fetch one record and return result
-        account = cursor.fetchone()
-        # If account exists in accounts table in out database
-        if account:
-            # Create session data, we can access this data in other routes
-            session['loggedin'] = True
-            # session['id'] = account['id']
-            session['UserName'] = account['UserName']
-            return redirect(url_for('home'))
-        else:
-            # Account doesnt exist or username/password incorrect
-            msg = 'Incorrect username/password!'
-    # Show the login form with message (if any)
-    return render_template('index.html', msg=msg)
-
-
-@app.route('/logout')
-def logout():
-    # Remove session data, this will log the user out
-   session.pop('loggedin', None)
-   session.pop('id', None)
-   session.pop('UserName', None)
-   # Redirect to login page
-   return redirect(url_for('login'))
+    return redirect(url_for('login'))
 
 
 # http://localhost:5000/Falsk/register - this will be the registration page, we need to use both GET and POST requests
@@ -120,30 +61,30 @@ def logout():
 def register():
     # Output message if something goes wrong...
     msg = ''
-
-        # Check if "username", "password" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'UserName' in request.form and 'FirstName' in request.form and 'LastName' in request.form and 'HashPwd' in request.form and 'UserType' in request.form:
+    # Check if "username", "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' and 'firstname' and 'lastname' and 'password' and 'usertype' in request.form:
         # Create variables for easy access
-        firstname = request.form['FirstName']
-        lastname = request.form['LastName']
-        username = request.form['UserName']
-        password = request.form['HashPwd']
-        usertype = request.form['UserType']
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        username = request.form['username']
+        hashPassword = sha256(request.form['password'].encode())
+        usertype = request.form['usertype']
 
         # Check if account exists using MySQL
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM USER WHERE UserName = %s', (username,))
+        cursor.execute('SELECT * FROM USER WHERE UserName = \"%s\"' % username)
         account = cursor.fetchone()
         # If account exists show error and validation checks
         if account:
             msg = 'Account already exists!'
         elif not re.match(r'[A-Za-z0-9]+', username):
             msg = 'Username must contain only characters and numbers!'
-        elif not username or not password:
+        elif not username or not hashPassword:
             msg = 'Please fill out the form!'
         else:
+            print("adding to database")
         # Account doesnt exists and the form data is valid, now insert new account into accounts table
-            cursor.execute('INSERT INTO USER (UserName, FirstName, LastName,HashPwd, UserType) VALUES ( %s, %s, %s, %s, %s)', (username, firstname, lastname, password, usertype,))
+            cursor.execute('INSERT INTO USER (UserName, FirstName, LastName,HashPwd, UserType) VALUES ( \"%s\", \"%s\", \"%s\", \"%s\", \"%s\")' % (username, firstname, lastname, hashPassword, usertype))
             mysql.connection.commit()
             msg = 'You have successfully registered!'
 
